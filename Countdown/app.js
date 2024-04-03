3355ac12fe0122b68e7fae741de0c8f4116e4f61
Pranali Gulhane <147273476+pranaligulhane@users.noreply.github.com>
2024-04-03 06:36:37 -0700

    Add files via upload

---
+++ Countdown/app.js
@@ -0,0 +1,23 @@
+
+const countdownDate = new Date('2024-05-01T00:00:00').getTime();
+
+
+const countdownTimer = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+ 
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  document.getElementById('timer').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+
+  if (distance < 0) {
+    clearInterval(countdownTimer);
+    document.getElementById('timer').innerHTML = 'EXPIRED';
+  }
+}, 1000);
